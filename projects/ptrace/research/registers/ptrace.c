cb9a84278d51c1ff11f7b9c53c0bba853158c64d
mkretkowski131 <318032@stud.umk.pl>
2023-11-08 15:18:07 +0100

    .

--- projects/ptrace/research/registers/ptrace.c
+++ projects/ptrace/research/registers/ptrace.c
@@ -6,7 +6,18 @@
 #include <sys/user.h>
 
 int main(){
-    
+    int pid, status;
+    long orig_rax;
+
+    if(pid == 0){
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        execl("./reg", "reg", 0);
+    }else{
+
+        wait(&status);
+        ptrace(PTRACE_SINGLESTEP, pid, 0, 0);
+
+    }
 
 
     return 0;
